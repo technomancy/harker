1aa1fe7884175bb47d82f21e0da663f1f3bbd10b
Phil Hagelberg <technomancy@gmail.com>
2009-03-30 16:57:19 -0700

    Fix gemification templates.

--- lib/harker/gemify.rb
+++ lib/harker/gemify.rb
@@ -4,9 +4,10 @@ require 'erb'
 
 module Harker
   # Turn an existing Rails app into a gem.
-  def self.gemify(rails_root, project_name = File.basename(rails_root))
-    if File.exist?(rails_root + "/bin/#{project_name}") or
-        File.exist?(rails_root + "/lib/#{project_name}.rb")
+  def self.gemify(rails_root)
+    @project_name = File.basename(rails_root)
+    if File.exist?(rails_root + "/bin/#{@project_name}") or
+        File.exist?(rails_root + "/lib/#{@project_name}.rb")
       abort "Can't write gem files without overwriting existing ones.
 Try manually gemifying."
     end
@@ -18,13 +19,13 @@ Try manually gemifying."
       puts "Added hoe block to Rakefile."
     end
 
-    File.open(File.join(rails_root, "/bin/#{project_name}"), 'w') do |fp|
-      FileUtils.mkdir_p(File.join(rails_root, '/bin'))
+    FileUtils.mkdir_p(File.join(rails_root, '/bin'))
+    File.open(File.join(rails_root, "/bin/#{@project_name}"), 'w') do |fp|
       fp.puts template('bin')
       puts "Wrote bin launcher."
     end
 
-    File.open(File.join(rails_root, "/lib/#{project_name}.rb"), 'w') do |fp|
+    File.open(File.join(rails_root, "/lib/#{@project_name}.rb"), 'w') do |fp|
       fp.puts template('lib')
       puts "Wrote lib file."
     end
@@ -39,6 +40,6 @@ Try manually gemifying."
 
   def self.template(name)
     template_path = File.join File.dirname(__FILE__), 'templates', "#{name}.erb"
-    ERB.new(template_path).result(binding)
+    ERB.new(File.read(template_path)).result(binding)
   end
 end
---
+++ test/test_gemify.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# The gemification step is tricky to test. =\
+
+sudo gem uninstall -x dummy
+rm -rf /tmp/dummy-instance
+rm -rf /tmp/dummy
+
+cd /tmp
+rails dummy
+cd /tmp/dummy
+script/generate scaffold Dummy name:string favourite_dumb_thing:string
+harker
+rake install_gem
+dummy init /tmp/dummy-instance
+cd /tmp/dummy-instance
+dummy migrate
+dummy foreground
